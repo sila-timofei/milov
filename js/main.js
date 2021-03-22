7bd4f01f457ee84e08447c78dfe3e5fe5aa9a6e5
Mac <mac@192-168-0-105.local>
2021-03-22 23:01:51 +0300

    off links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 window.addEventListener('DOMContentLoaded', function() {
     var btnMenu = document.querySelector('.btn-menu'),
         menu = document.querySelector('.nav-links'),
-        lineMenu = document.querySelectorAll('.btn-menu__line-animation');
+        lineMenu = document.querySelectorAll('.btn-menu__line-animation'),
+        turnActivity = document.querySelectorAll('.off-link');
 
         btnMenu.addEventListener('click', () => {
             menu.classList.toggle('show-menu');
             lineMenu[0].classList.toggle('btn-menu__one-line');
             lineMenu[1].classList.toggle('btn-menu__one-line1');
+        });
+
+        turnActivity.forEach(i => {
+            i.addEventListener('click', (e) => {
+                e.preventDefault();
+            });
         });
 });
